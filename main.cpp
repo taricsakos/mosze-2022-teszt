3ab39b416f90f9c68e981a9ac8c540e188f44a0a
taricsakos <taricsakos@gmail.com>
2022-09-12 19:11:39 +0200

    errors

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // "_" hiányzik a változónévből
+    std::cout << '1-100 ertekek duplazasa' // nincs sorvégi zárás
+    for (int i = 0;) // hibás ciklus megadása, több elem hiánya
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  // rossz felsőhatár
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // eredménykiírás nem történik meg, nincs sorvégi zárás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;  // inicializálás nélküli változó megadása
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
